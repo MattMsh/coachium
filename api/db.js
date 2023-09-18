05c3964b879d5c077504e224dcc725c9d344abac
Matvii Mashtaliar <matvii.mashtalyar@gmail.com>
2023-09-18 20:07:22 +0300

    fix: add end to configuration

--- api/db.js
+++ api/db.js
@@ -1,11 +1,14 @@
-import { Sequelize } from 'sequelize';
+import dotenv from "dotenv";
+dotenv.config();
+import { Sequelize } from "sequelize";
 
 export default new Sequelize(
-    'coachium',
-    'postgres',
-    'postgres',
-    {
-        dialect: 'postgres',
-        host: 'localhost'
-    },
-)
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+);
